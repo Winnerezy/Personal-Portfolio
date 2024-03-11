92cc7a81c8deaaf12ace367bd0a7949dac279bc3
Yuki2no <eragbonikpeya@gmail.com>
2024-03-11 14:35:32 +0100

    [Update] - Added Cards - Replaced background color

--- porfolio/src/components/card.jsx
+++ porfolio/src/components/card.jsx
@@ -1,8 +1,11 @@
 export default function Card(props){
 
     return (
-        <div>
+        <div className="w-80 h-96 border-4 border-white rounded-2xl grid grid-cols-1 items-center justify-center pl-8 pr-8">
+            <img src={props.image} alt={props.title} />
             <div>{props.title}</div>
+            <span>{props.description}</span>
+            <span className="text-end">-- Created by Winner</span>
         </div>
     )
 }
--- porfolio/src/components/cardList.jsx
+++ porfolio/src/components/cardList.jsx
@@ -2,8 +2,28 @@ import Card from "./card";
 
 export default function CardList(){
     return(
+        <div className="grid grid-cols-1 items-center justify-center">
+            
+        <header className="underline text-3xl font-serif font-bold text-center mt-36">PROJECTS</header>
+
+
+        <section className="flex items-center justify-evenly mt-24">
         <div>
-            <Card title= "Cali"/>
+            <Card title= "Cali" description= "Cali a simplistic calculator made with HTML, CSS and Javascript. Implementing the knowledge of basic arithmetic to solve problems" image=""/>
         </div>
+
+        <div>
+            <Card title= "Weather Me" description=""/>
+        </div>
+
+        <div>
+            <Card title= "Pomi" description= ""/>
+        </div>
+
+        </section>
+        </div>
+
+
     )
 }
+
